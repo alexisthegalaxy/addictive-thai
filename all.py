17d6bb32fd6f82f0d18b59585dd46f8be233ec71
Alexis Viand <alexis@memrise.com>
2019-02-10 18:13:51 +0000

    add main and all

---
+++ all.py
@@ -0,0 +1,37 @@
+from lexicon.dex import Dex
+from lexicon.items import Words, Sentences, Syllables
+from ui.ui import Ui
+
+
+class All:
+    def __init__(
+        self,
+        mas: 'Mas',
+        words: 'Words',
+        sentences: 'Sentences',
+        syllables: 'Syllables',
+        ui: 'Ui',
+        cell_types: 'CellType',
+        profiles: 'Profile',
+    ):
+        from lexicon.tests import Test
+
+        self.mas = mas
+        self.mas.al = self
+        self.words = words
+        self.sentences = sentences
+        self.syllables = syllables
+        self.ui: Ui = ui
+        self.learner = None
+        self.cell_types = cell_types
+        self.profiles = profiles
+        self.active_test: Test = None
+        self.active_npc: 'Npc' = None
+        self.active_learning = None
+        self.dex: Dex = None
+
+    def add_sentences_to_words(self):
+        for sentence in self.sentences.sentences:
+            for word in sentence.words:
+                word.sentences.append(sentence)
+
---
+++ main.py
@@ -0,0 +1,77 @@
+from all import All
+from lexicon.dex import Dex
+from lexicon.init import init_vocab
+import pygame
+
+from ow.learner import Learner
+from ow.overworld import Mas, CellTypes
+from profile.profile import Profiles
+from ui.ui import Ui
+
+
+def main():
+    syllables, words, sentences = init_vocab()
+    cell_types = CellTypes()
+    mas = Mas(words, cell_types)
+    mas.form_links()
+    profiles = Profiles()
+    profiles.set_as_profile("Alexis")
+    al = All(
+        mas=mas,
+        words=words,
+        sentences=sentences,
+        syllables=syllables,
+        ui=Ui(),
+        cell_types=cell_types,
+        profiles=profiles,
+    )
+
+    al.add_sentences_to_words()
+    al.learner = Learner(al, 1, 1, (150, 0, 150))
+    mas.import_npcs(al)
+
+    profiles.current_profile.load(al)
+    al.dex = Dex(al)
+    while al.ui.running:
+        al.ui.listen_event(al)
+        ow_frozen = False
+        if al.active_test:
+            ow_frozen = True
+            al.active_test.interact(al)
+        if al.dex.active:
+            ow_frozen = True
+            al.dex.interact()
+        if al.active_npc:
+            ow_frozen = True
+            if al.ui.space:
+                al.learner.start_interacting(al)
+        if al.active_learning:
+            ow_frozen = True
+            al.active_learning.interact(al)
+        if not ow_frozen:
+            if al.ui.space:
+                al.learner.start_interacting(al)
+            if al.learner.can_move():
+                al.learner.move(al)
+
+        al.ui.screen.fill((0, 0, 0))
+        al.mas.current_map.draw(al)
+        for npc in al.mas.current_map.npcs:
+            npc.draw(al)
+        al.learner.draw(al)
+        for npc in al.mas.current_map.npcs:
+            if not npc.active_line_index == -1:
+                npc.draw_text(al)
+        if al.active_test:
+            al.active_test.draw()
+        if al.active_learning:
+            al.active_learning.draw()
+        if al.dex.active:
+            al.dex.draw()
+        al.learner.draw_money_and_hp(al)
+        pygame.display.flip()
+        al.ui.clock.tick(50)
+
+
+if __name__ == "__main__":
+    main()
